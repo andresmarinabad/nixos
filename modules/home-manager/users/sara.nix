a069e01b38bdc55f3abbd5bd47797b1fdbe91e9c
Andres <andresmarinabad@protonmail.com>
2026-02-20 09:45:13 +0100

    feat: add sara plasma with wallpaper

--- modules/home-manager/users/sara.nix
+++ modules/home-manager/users/sara.nix
@@ -21,6 +21,48 @@
     ];
   };
 
+  programs.plasma = {
+    enable = true;
+
+    configFile.kdeglobals.General = {
+      TerminalApplication = "kitty";
+      TerminalService = "kitty.desktop";
+    };
+
+    # Workspace settings
+    workspace = {
+      lookAndFeel = "org.kde.breeze.desktop";
+      colorScheme = "BreezeClassic";
+      theme = "breeze";
+      cursor.theme = "Breeze";
+      iconTheme = "Breeze";
+      wallpaper = ../../../assets/img/wallpaper/tren.jpg;
+    };
+
+    kwin = {
+      virtualDesktops = {
+        rows = 1;
+        number = 2;
+        names = [
+          "Cole"
+          "Personal"
+        ];
+      };
+    };
+
+    input.keyboard = {
+      numlockOnStartup = "on";
+    };
+
+    # Fonts configuration
+    fonts = {
+      general = { family = "Inter"; pointSize = 10; };
+      fixedWidth = { family = "JetBrainsMono Nerd Font"; pointSize = 10; };
+      windowTitle = { family = "Kanit"; pointSize = 11; };
+    };
+
+  };
+
   home.stateVersion = "26.05"; 
 
 }

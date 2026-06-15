f82412dc45908a0233eb4ce26fc1c26614145ad6
Andrés <andresmarinabad@protonmail.com>
2026-06-15 21:56:14 +0200

    feat: power management settings

--- modules/home-manager/common.nix
+++ modules/home-manager/common.nix
@@ -19,4 +19,43 @@
   };
 
   home.stateVersion = "26.05";
+
+  programs.plasma.powerdevil = {
+    AC = {
+      displayBrightness              = 100;
+      autoSuspend.action             = "sleep";
+      autoSuspend.idleTimeout        = 900;   # 15 min
+      dimDisplay.enable              = true;
+      dimDisplay.idleTimeout         = 300;   # 5 min
+      turnOffDisplay.idleTimeout     = 600;   # 10 min
+      turnOffDisplay.idleTimeoutWhenLocked = 60; # 1 min
+      powerButtonAction              = "sleep";
+      whenLaptopLidClosed            = "doNothing";
+      powerProfile                   = "balanced";
+    };
+    battery = {
+      displayBrightness              = 75;
+      autoSuspend.action             = "sleep";
+      autoSuspend.idleTimeout        = 300;   # 5 min
+      dimDisplay.enable              = true;
+      dimDisplay.idleTimeout         = 120;   # 2 min
+      turnOffDisplay.idleTimeout     = 300;   # 5 min
+      turnOffDisplay.idleTimeoutWhenLocked = 60; # 1 min
+      powerButtonAction              = "sleep";
+      whenLaptopLidClosed            = "sleep";
+      powerProfile                   = "balanced";
+    };
+    lowBattery = {
+      displayBrightness              = 50;
+      autoSuspend.action             = "sleep";
+      autoSuspend.idleTimeout        = 300;   # 5 min
+      dimDisplay.enable              = true;
+      dimDisplay.idleTimeout         = 60;    # 1 min
+      turnOffDisplay.idleTimeout     = 120;   # 2 min
+      turnOffDisplay.idleTimeoutWhenLocked = 60; # 1 min
+      powerButtonAction              = "sleep";
+      whenLaptopLidClosed            = "sleep";
+      powerProfile                   = "powerSaving";
+    };
+  };
 }
